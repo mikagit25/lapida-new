8afd7fa949044184d636721dc4579237aa77a1c8
mikagit25 <mikalai.github@gmail.com>
2025-07-30 22:15:13 +0300

    Добавлены настройки CORS для lapida.one и подробная инструкция по хостингу

--- server/app.js
+++ server/app.js
@@ -8,7 +8,25 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5180', 'http://localhost:5181', 'http://localhost:5182', 'http://localhost:5183', 'http://localhost:5184', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'http://localhost:5177', 'http://localhost:5178', 'http://localhost:5179', 'http://localhost:3000'],
+  origin: [
+    'http://localhost:5173', 
+    'http://localhost:5180', 
+    'http://localhost:5181', 
+    'http://localhost:5182', 
+    'http://localhost:5183', 
+    'http://localhost:5184', 
+    'http://localhost:5174', 
+    'http://localhost:5175', 
+    'http://localhost:5176', 
+    'http://localhost:5177', 
+    'http://localhost:5178', 
+    'http://localhost:5179', 
+    'http://localhost:3000',
+    'https://lapida.one',
+    'https://www.lapida.one',
+    'http://lapida.one',
+    'http://www.lapida.one'
+  ],
   credentials: true
 }));
 app.use(express.json());
