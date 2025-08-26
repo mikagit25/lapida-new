ed92ecb8ea656f02c56f0db77e636311415eea23
mikagit25 <mikalai.github@gmail.com>
2025-08-26 22:56:09 +0300

    chore: remove hardcoded Google OAuth secrets for GitHub push protection

--- server/oauth.js
+++ server/oauth.js
@@ -8,29 +8,30 @@ const User = require('./models/User');
 
 // --- OAuth стратегии временно отключены ---
 // Google OAuth
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID || '164850732815-8nbcflgvevpgoqmvlpriq9ni9dkh6uc5.apps.googleusercontent.com',
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET || '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',
-  callbackURL: '/api/auth/google/callback',
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    const email = profile.emails[0].value;
-    let user = await User.findOne({ email });
-    if (!user) {
-      user = await User.create({
-        name: profile.displayName,
-        email,
-        avatar: profile.photos[0]?.value,
-        googleId: profile.id,
-        password: Math.random().toString(36).slice(-8), // случайный пароль
-        isVerified: true,
-      });
-    }
-    return done(null, user);
-  } catch (err) {
-    return done(err, null);
-  }
-}));
+// Google OAuth временно отключён. Для включения используйте только переменные окружения.
+// passport.use(new GoogleStrategy({
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL: '/api/auth/google/callback',
+// }, async (accessToken, refreshToken, profile, done) => {
+//   try {
+//     const email = profile.emails[0].value;
+//     let user = await User.findOne({ email });
+//     if (!user) {
+//       user = await User.create({
+//         name: profile.displayName,
+//         email,
+//         avatar: profile.photos[0]?.value,
+//         googleId: profile.id,
+//         password: Math.random().toString(36).slice(-8), // случайный пароль
+//         isVerified: true,
+//       });
+//     }
+//     return done(null, user);
+//   } catch (err) {
+//     return done(err, null);
+//   }
+// }));
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
