19d0afe3023b5f1ef845719c21a63ee0e28d52ed
mikagit25 <mikalai.github@gmail.com>
2025-09-05 12:34:22 +0300

    refactor: replace all fetch calls with apiFetch for global auth

---
+++ client/src/services/apiFetch.js
@@ -0,0 +1,9 @@
+// Универсальная обёртка для fetch с автоматическим добавлением Authorization
+export async function apiFetch(url, options = {}) {
+  const token = localStorage.getItem('authToken');
+  const headers = {
+    ...(options.headers || {}),
+    ...(token ? { Authorization: `Bearer ${token}` } : {}),
+  };
+  return fetch(url, { ...options, headers });
+}
